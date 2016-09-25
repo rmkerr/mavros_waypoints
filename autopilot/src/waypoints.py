1f2f733d9e75a0bb58f499ea9e037d150f1cadda
Max Kerr <richard.m.kerr@vanderbilt.edu>
2016-09-25 15:10:00 -0500

    Added wwaypoint support

--- autopilot/src/waypoints.py
+++ autopilot/src/waypoints.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import rospy
 import roslib
-from geometry_msgs.msg import PoseStamped
+import os
 from mavros_msgs.msg import *
 from mavros_msgs.srv import *
 
@@ -21,27 +21,15 @@ state_sub = rospy.Subscriber("/mavros/state", State, state_callback)
 arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)
 set_mode_client = rospy.ServiceProxy("mavros/set_mode", SetMode)
 
-waypoint_service = rospy.ServiceProxy("mavros/mission/push", WaypointPush)
-
 rate = rospy.Rate(20.0)
 
 while current_state and current_state.connected:
     rate.sleep()
 
-waypoint = Waypoint()
-waypoint.x_lat = 20
-waypoint.y_long = 20
-waypoint.z_alt = 2
-waypoint.is_current = True
-waypoint.command = 16
-
+os.system("rosrun mavros mavwp load //home//$USER//local_ws//waypoints.txt")
 
 arming_client(True)
 set_mode_client(custom_mode="AUTO")
-
-while True:
-    waypoint_service(waypoint)
-    rate.sleep()
 
 try:
     rospy.spin()
